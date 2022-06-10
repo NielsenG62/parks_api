bca1454982dc6e4bd1b85f5d4e7506676cb574de
Gabe Nielsen <gabenielsen7@gmail.com>
2022-06-10 16:10:30 -0700

    WIP: cannot post user to authenticate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_220834) do
+ActiveRecord::Schema.define(version: 2022_06_10_225733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parks", force: :cascade do |t|
     t.string "park_name"
