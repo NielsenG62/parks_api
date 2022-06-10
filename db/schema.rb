fcc2b036c179844e0708a13d95654902ea82e25a
Gabe Nielsen <gabenielsen7@gmail.com>
2022-06-10 15:20:08 -0700

    WIP: attempting authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_163001) do
+ActiveRecord::Schema.define(version: 2022_06_10_220834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2022_06_10_163001) do
     t.integer "climb_pitches"
     t.integer "climb_height_feet"
     t.integer "park_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
