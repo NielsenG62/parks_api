fafc932651ceecc5b7378ca8a593036c7ee656f8
Gabe Nielsen <gabenielsen7@gmail.com>
2022-06-10 10:36:47 -0700

    update response

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,5 +1,9 @@
 module Response
   def parks_json_response(object, status = :ok)
-    render json: object.to_json(include: :sport_climbs, :trad_climbs), status:status
+    render json: object.to_json(include: [:sport_climbs, :trad_climbs]), status:status
+  end
+
+  def climbs_json_response(object, status = :ok)
+    render json: object, status: status
   end
 end
