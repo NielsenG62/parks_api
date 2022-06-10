f4cb1226ccb479cbd458dccfef025fdcb38b759d
Gabe Nielsen <gabenielsen7@gmail.com>
2022-06-10 11:48:08 -0700

    add validation

--- app/models/park.rb
+++ app/models/park.rb
@@ -3,4 +3,7 @@ class Park < ApplicationRecord
   has_many :trad_climbs
   accepts_nested_attributes_for :sport_climbs
   accepts_nested_attributes_for :trad_climbs
+
+  validates :park_name, presence: true
+  validates :park_city, presence: true
 end
--- app/models/trad_climb.rb
+++ app/models/trad_climb.rb
@@ -1,3 +1,7 @@
 class TradClimb < ApplicationRecord
   belongs_to :park
+
+  validates :climb_name, presence: true
+  validates :climb_grade, presence: true
+  validates :park_id, presence: true
 end
