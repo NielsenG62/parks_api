cc1eb3334c1f8af5bb4585a60f026648dff91644
Gabe Nielsen <gabenielsen7@gmail.com>
2022-06-10 09:57:13 -0700

    add parks controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,6 +1,31 @@
 class ParksController < ApplicationController
   def index
-    @parks = 
+    @parks = Park.all
     parks_json_response(@parks)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    parks_json_response(@park)
+  end
+
+  def create
+    @park = Park.create!(park_params)
+    parks_json_response(@park)
+  end
+
+  def update
+    @park = Park.find(params[:park_id])
+    @park.update(park_params)
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    @park.destroy
+  end
+
+  private
+  def park_params
+    params.permit(:park_name, :park_city)
   end
 end
